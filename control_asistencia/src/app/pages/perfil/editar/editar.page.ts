2a4ff336feb74f560008c09ba5177d2c8f3b13ed
ElAdminExe <thomas.rene.q.e@gmail.com>
2023-09-06 17:59:58 -0300

    editar

--- control_asistencia/src/app/pages/perfil/editar/editar.page.ts
+++ control_asistencia/src/app/pages/perfil/editar/editar.page.ts
@@ -9,13 +9,30 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class EditarPage implements OnInit {
   id:number = 0;
-
+  nombre:string = this.userService.getNombre(this.id);
+  apellido:string = this.userService.getApellido(this.id);
+  contrasenia:string = this.userService.getContrasenia(this.id);
   constructor(private router:Router, private userService: UserService, private activatedRoute:ActivatedRoute) { }
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.params['id'];
   }
-  aceptar(){
 
+  getNombre(): string {
+    return `${this.userService.getNombre(this.id)}`;
+  }
+  getApellido(): string {
+    return `${this.userService.getApellido(this.id)}`;
+  }
+  getEmail(): string {
+    return `${this.userService.getEmail(this.id)}`;
+  }
+  getContrasenia(): string {
+    return `${this.userService.getContrasenia(this.id)}`;
+  }
+
+
+  actualizar(){
+    this.userService.updateUser(this.id, this.nombre, this.apellido, this.contrasenia);
   }
   volver(){
     this.router.navigateByUrl("perfil/"+this.id);

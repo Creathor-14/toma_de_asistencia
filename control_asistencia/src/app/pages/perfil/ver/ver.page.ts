6e193f233ae8a2d14790c68e9cb873258e52a255
ElAdminExe <thomas.rene.q.e@gmail.com>
2023-09-06 21:53:13 -0300

    animaciones + skeleton en perfil

--- control_asistencia/src/app/pages/perfil/ver/ver.page.ts
+++ control_asistencia/src/app/pages/perfil/ver/ver.page.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { trigger,state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-ver',
   templateUrl: './ver.page.html',
   styleUrls: ['./ver.page.scss'],
+  animations: [
+    trigger('fadeInOut',[
+      state('void', style({ opacity: 0})),
+      state('*', style({ opacity: 1})),
+      transition('void => *', animate('1000ms ease-in')),
+      transition('* => void', animate('1000ms ease-out')),
+    ])
+  ]
 })
 export class VerPage implements OnInit {
+  loading = true;
   id:number = 0;
 
   constructor(private router:Router, private userService: UserService, private activatedRoute:ActivatedRoute) { }
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.params['id'];
+    setTimeout(() => {
+      this.loading = false;
+    },2000);
   }
   getNombre(): string {
     return `${this.userService.getNombre(this.id)}`;

6a1b45576d774ff85cdb2843638cb7ed04334e72
Mredo13 <114360114+Mredo13@users.noreply.github.com>
2023-10-11 19:16:15 -0300

    alert

--- control_asistencia/src/app/pages/sesion/login/password/password.page.ts
+++ control_asistencia/src/app/pages/sesion/login/password/password.page.ts
@@ -3,6 +3,8 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HelperService } from 'src/app/services/helper.service';
 import { UserService } from 'src/app/services/user.service';
+import { StorageService } from 'src/app/services/storage.service';
+
 
 
 @Component({
@@ -11,49 +13,61 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./password.page.scss'],
 })
 export class PasswordPage implements OnInit {
-  email:string=this.userService.getActualEmail();
-  contrasenia:string = "";
+  email: string = this.userService.getActualEmail();
+  
+  contrasenia: string = "";
 
-  constructor(private router:Router, private userService:UserService, private activatedRoute:ActivatedRoute, 
-    private auth: AngularFireAuth, private helperService:HelperService) { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute,
+    private auth: AngularFireAuth,
+    private helperService: HelperService,
+    private storageService:StorageService
+  ) {}
+
   ngOnInit() {
-    //this.id = this.activatedRoute.snapshot.params['id'];
-    //this.id = this.userService.getActualId();
     this.email = this.activatedRoute.snapshot.params['email'];
   }
 
   getMensajeBienvenida(): string {
-    //return `${this.userService.getEmail(this.id)}`;
     return this.email;
   }
-  login(){
-    this.auth.signInWithEmailAndPassword(this.email,this.contrasenia)
-    .then(response => {
-      this.helperService.showAlert("User successfully Logged.","Succes");
-      this.router.navigateByUrl("tabs/"+this.email);
-      console.log(response);
-    })
-    .catch(error => {
 
-      if(error.code == "auth/missing-password"){
-        this.helperService.showAlert("Ingrese una contraseña.", "Error de validación");
-      }else if(error.code == "auth/invalid-login-credentials"){
-        this.helperService.showAlert("Usuario inexistente o datos invalidos.", "Error de validación");
-        //this.router.navigateByUrl("login/user");
-      }else{
-        console.log(error)
-      }
-    });
-    /*
-    var respuesta = this.userService.contraseniaCorrecta(this.id, this.contrasenia);
-    if (respuesta!=-1){
-      this.router.navigateByUrl("tabs/"+this.id);
-    }*/
+  login() {
+    this.auth
+      .signInWithEmailAndPassword(this.email, this.contrasenia)
+      .then((response) => {
+        this.mostrarMensajeInicioSesionExitoso();
+        this.router.navigateByUrl("tabs/" + this.email);
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.code == "auth/missing-password") {
+          this.helperService.showAlert("Ingrese una contraseña.", "Error de validación");
+        } else if (error.code == "auth/invalid-login-credentials") {
+          this.helperService.showAlert(
+            "Usuario inexistente o datos inválidos.",
+            "Error de validación"
+          );
+        } else {
+          console.error(error);
+        }
+      });
   }
-  volver(){//pasar el correo por parametro
+
+  mostrarMensajeInicioSesionExitoso() {
+    
+    const usuario = this.email;
+    const mensaje = `¡Bienvenido, ${usuario} !`;
+    this.helperService.showAlert(mensaje, "Inicio de Sesión Exitoso");
+  }
+
+  volver() {
     this.router.navigateByUrl("login/user");
   }
-  recuperar(){
-    this.router.navigateByUrl("recuperar/"+this.email);
+
+  recuperar() {
+    this.router.navigateByUrl("recuperar/" + this.email);
   }
 }

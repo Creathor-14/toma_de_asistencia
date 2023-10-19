03409c3e05716eae8037cd37ddacba1a5dfb89d4
Mredo13 <114360114+Mredo13@users.noreply.github.com>
2023-10-19 11:54:33 -0300

    ValidacionRegistro

--- control_asistencia/src/app/pages/sesion/registrar/registrar.page.ts
+++ control_asistencia/src/app/pages/sesion/registrar/registrar.page.ts
@@ -4,11 +4,9 @@ import { Router } from '@angular/router';
 import { HelperService } from 'src/app/services/helper.service';
 import { StorageService } from 'src/app/services/storage.service';
 import { UserService } from 'src/app/services/user.service';
-
 import { ApisService } from 'src/app/services/apis.service';
 import { Region } from 'src/app/models/region';
 import { User } from 'src/app/models/user.model';
-
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.page.html',
@@ -21,43 +19,36 @@ export class RegistrarPage implements OnInit {
   region: string = "";
   comuna: string = "";
   password: string ="";
-
   constructor(private userService: UserService,private router:Router, private auth: AngularFireAuth, private helperService:HelperService,
     private storageService:StorageService, private apisService:ApisService) {
-    
   }
-
   volver(){
     this.router.navigateByUrl("login/user");
   }
   ngOnInit() {
     this.cargarRegion();
   }
-  
-  
-  addUser(){
-
-    this.region = this.apisService.getNombreUbicacion(this.regionSel,this.regiones);
-    this.comuna = this.apisService.getNombreUbicacion(this.comunaSel,this.comunas);
-  
-    if (this.nombre == null) {
-      this.helperService.showAlert("Debe ingresar un nombre.", "Advertencia");
-    } else if (this.apellido == null) {
-      this.helperService.showAlert("Debe ingresar un apellido.", "Advertencia");
-    } else if(this.regionSel == -1){
-      this.helperService.showAlert("Debe ingresar una region.", "Advertencia");
-    } else if(this.comunaSel == -1){
-      this.helperService.showAlert("Debe ingresar una comuna.", "Advertencia");
+  addUser() {
+    if (!this.email || !this.nombre || !this.apellido || this.regionSel === -1 || this.comunaSel === -1 || !this.password) {
+      this.helperService.showAlert("Por favor, complete todos los campos.", "Error de validación");
+    } else if (this.password.length >= 6 && this.password.length >= 20) {
+      this.helperService.showAlert("La contraseña debe tener entre 6 y 20 caracteres.", "Error de validación");
     } else {
+      this.region = this.apisService.getNombreUbicacion(this.regionSel, this.regiones);
+      this.comuna = this.apisService.getNombreUbicacion(this.comunaSel, this.comunas);
       this.helperService.showLoader("Cargando").then(loader => {
         try {
           this.auth.createUserWithEmailAndPassword(this.email, this.password)
             .then(response => {
-              this.helperService.showAlert("Usuario registrado correctamente.", "Success");
-              let user:User = { 
-                email:this.email, nombre:this.nombre, apellido:this.apellido, 
-                region: this.region, comuna: this.comuna,
-                contrasenia:this.password,asistencias:[] 
+              this.helperService.showAlert("Usuario registrado correctamente.", "Éxito");
+              let user: User = {
+                email: this.email,
+                nombre: this.nombre,
+                apellido: this.apellido,
+                region: this.region,
+                comuna: this.comuna,
+                contrasenia: this.password,
+                asistencias: []
               };
               this.storageService.guardarUser(user);
             })
@@ -72,13 +63,11 @@ export class RegistrarPage implements OnInit {
                 this.helperService.showAlert("Ingrese un correo.", "Error de validación");
               } else if (error.code == 'auth/missing-password') {
                 this.helperService.showAlert("Ingrese una contraseña.", "Error de validación");
-              }else if(error.code == "auth/network-request-failed"){
-                this.helperService.showAlert("Problema de conectividad con los servidores un problema en la red", "Error de red");
-
-              }
-              else {
+              } else if (error.code == "auth/network-request-failed") {
+                this.helperService.showAlert("Problema de conectividad con los servidores, verifique su conexión a Internet.", "Error de red");
+              } else {
                 this.helperService.showAlert(error, "Error");
-                console.log(error);
+                console.error(error);
               }
             })
             .finally(() => {
@@ -100,21 +89,14 @@ export class RegistrarPage implements OnInit {
       
     } catch (error:any) {
       console.log("ERROR", error);
-      
       this.helperService.showAlert(error.error.msg,"Error")
     }
   }
-
   async cargarRegion(){
     try {
       const req = await this.apisService.getRegion();
       this.regiones = req.data;
-      
-      
     } catch (error) {
-      
     }
   }
-
-  
 }
